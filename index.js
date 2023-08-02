0fd1d6c1f7f2c999b217959b500e5983ca7abd2a
faisalsaid <falsdesain@gmail.com>
2023-08-02 21:50:56 +0900

    module setup

--- index.js
+++ index.js
@@ -1,12 +1,7 @@
-require('dotenv').config();
 const express = require('express')
-const mongose = require('mongose')
 const app = express()
-
-
 app.all('/', (req, res) => {
-    console.log("Hallo World")
-    res.send('Just Simple')
+    console.log("Just got a request!")
+    res.send('Upadate')
 })
 app.listen(process.env.PORT || 3000)
-
