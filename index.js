3b037e19a78367ae03b2a904e3490a12e44a7718
Faisal Said <61022946+faisalsaid@users.noreply.github.com>
2023-10-24 17:31:17 +0900

    test update

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 const data = process.env.DATA
-app.all('/', (req, res) => {
+app.all('/api', (req, res) => {
     console.log("Just got a request!")
-    res.send('data.toString()')
+    res.send('api is live')
 })
 app.listen(process.env.PORT || 3000)
