1b92974139f7476bef81b76e61f6f27d4b554cae
faisalsaid <falsdesain@gmail.com>
2023-10-24 19:58:34 +0900

    conect to mongodb

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 require('dotenv').config()
 const express = require('express')
-const app = express()
+const mongoose = require('mongoose')
+
 const data = process.env.DATA
 const port = process.env.PORT || 3000
+
+mongoose.connect(process.env.MONGO_DB)
+
+const app = express()
 
 app.all('/api', (req, res) => {
     console.log("Just got a request!")
