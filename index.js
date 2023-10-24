5c691e35bc004446445642a9f6319a208077aad8
faisalsaid <falsdesain@gmail.com>
2023-10-24 19:24:45 +0900

    add nodemon

--- index.js
+++ index.js
@@ -2,8 +2,12 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 const data = process.env.DATA
+const port = process.env.PORT || 3000
+
 app.all('/api', (req, res) => {
     console.log("Just got a request!")
     res.send('api is live')
 })
-app.listen(process.env.PORT || 3000)
+app.listen(port, () => {
+    console.log(`Server is runing on port ${port}`);
+})
