faa557079acb150856898ba19f526ec0672bcc9c
faisalsaid <falsdesain@gmail.com>
2023-11-15 13:31:45 +0900

    make route dashboard protected

--- routes/dashboard.route.js
+++ routes/dashboard.route.js
@@ -4,6 +4,6 @@ const { getAllPurchase } = require('../controllers/dashboard.controllers.js');
 
 const router = express.Router();
 
-router.route('/').get(getAllPurchase);
+router.route('/').get(protected, getAllPurchase);
 
 module.exports = router;
